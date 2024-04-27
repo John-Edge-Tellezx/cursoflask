fe780b56899b3d2531c2d6ff1dac1aec7cd13a03
Tellezx <johnedgetellezx@gmail.com>
2024-04-27 17:21:17 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import render_template
+@app.route('/')
+@app.route('/index')
+def index():
+    nome = "Tellezx"
+    dados = {"profissao": "Estudante", "canal": "Student. Tellezx"}
+    return render_template('index.html', nome=nome, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
